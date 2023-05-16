04db5c8436b9aa006bf43a1e363e3d2f5ec55823
maoyansong <maoyansong@maoyansongs-MacBook-Pro.local>
2023-05-16 23:18:57 +0800

    init super app stub for iOS

---
+++ SuperAppiOS/SuperAppiOS/SuperAppiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperAppiOSApp.swift
+//  SuperAppiOS
+//
+//  Created by 毛岩松 on 2023/5/16.
+//
+
+import SwiftUI
+
+@main
+struct SuperAppiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
