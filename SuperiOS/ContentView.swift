636302bc4996a3bf8a6d3cd38fd6c2f645018c66
Zhu, Bart <zhuchen@zhuchens-iMac.lan>
2023-05-18 10:24:45 +0800

    add iOS empty project

---
+++ SuperiOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SuperiOS
+//
+//  Created by 朱琛 on 2023/5/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SuperiOS/SuperiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperiOSApp.swift
+//  SuperiOS
+//
+//  Created by 朱琛 on 2023/5/18.
+//
+
+import SwiftUI
+
+@main
+struct SuperiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
